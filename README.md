0c3e70aa8443067439083524eb58af8e51f9fa2e
George <levchenko.george.vata@gmail.com>
2025-03-24 11:41:21 +0100

    Home page video fixed

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Improvements list (didn't have enough time to finish):
 - Implement pagination / virtual Scroll for listing
-- Way to skip browser policy and launch video from start
+
--- src/app/containers/home/home.component.ts
+++ src/app/containers/home/home.component.ts
@@ -1,9 +1,10 @@
-import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, inject, viewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, inject, signal, viewChild } from '@angular/core';
 import { Button } from 'primeng/button';
 import { Router } from '@angular/router';
 import { TranslocoPipe } from '@jsverse/transloco';
 import { TitleCasePipe } from '@angular/common';
 import { CapitalizePipe } from '../../utils/pipes/capitalize/capitalize.pipe';
+import { catchError, EMPTY, from, fromEvent, tap } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -15,19 +16,42 @@ import { CapitalizePipe } from '../../utils/pipes/capitalize/capitalize.pipe';
 export class HomeComponent implements AfterViewInit {
   private readonly router = inject(Router);
 
+  readonly isVideoPlaying = signal<boolean>(false);
   private readonly video = viewChild<ElementRef<HTMLVideoElement>>('video');
 
   goToBikes(): void {
     this.router.navigate(['/bikes']);
   }
 
-  ngAfterViewInit() {
-    this.video()!
-      .nativeElement.play()
-      .catch(() => {
-        // console.warn('Autoplay was prevented:', error);
-        // Known issue: on the first landing without user interactions video wouldn't be played
-        // https://developer.chrome.com/blog/autoplay/
-      });
+  playVideo(): void {
+    from(this.video()!.nativeElement.play())
+      .pipe(
+        tap(() => this.isVideoPlaying.set(true)),
+        catchError(error => {
+          console.error('Manual play prevented:', error);
+          return EMPTY;
+        })
+      )
+      .subscribe();
+  }
+
+  ngAfterViewInit(): void {
+    // Attempt autoplay on landing.
+    from(this.video()!.nativeElement.play())
+      .pipe(
+        tap(() => this.isVideoPlaying.set(true)),
+        catchError(error => {
+          console.warn('Autoplay was prevented:', error);
+          // Known issue: on the first landing without user interactions video wouldn't be played
+          // https://developer.chrome.com/blog/autoplay/
+          return EMPTY;
+        })
+      )
+      .subscribe();
+
+    // Listen for the 'play'
+    fromEvent(this.video()!.nativeElement, 'play')
+      .pipe(tap(() => this.isVideoPlaying.set(true)))
+      .subscribe();
   }
 }
