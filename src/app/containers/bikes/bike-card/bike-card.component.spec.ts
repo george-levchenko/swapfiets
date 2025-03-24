52660245f9eec65c87846f0850c18e036a20cec5
George <levchenko.george.vata@gmail.com>
2025-03-24 10:56:09 +0100

    test fix

--- src/app/containers/bikes/bike-card/bike-card.component.spec.ts
+++ src/app/containers/bikes/bike-card/bike-card.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { BikeCardComponent } from './bike-card.component';
+import { Bike } from '../../../models/interfaces/bike.interface';
 
 describe('BikeCardComponent', () => {
   let component: BikeCardComponent;
@@ -10,9 +10,23 @@ describe('BikeCardComponent', () => {
     await TestBed.configureTestingModule({
       imports: [BikeCardComponent],
     }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(BikeCardComponent);
     component = fixture.componentInstance;
+    // Provide a valid Bike object
+    const fakeBike: Bike = {
+      id: 1,
+      thumb: '/bike1.png',
+      large_img: '/bike1-large.png',
+      title: 'Bike 1',
+      stolen_location: 'Amsterdam Center',
+      manufacturer_name: 'BikeCo',
+      frame_model: 'Model X',
+      frame_colors: ['red', 'blue'],
+    };
+    fixture.componentRef.setInput('bike', fakeBike);
     fixture.detectChanges();
   });
 
